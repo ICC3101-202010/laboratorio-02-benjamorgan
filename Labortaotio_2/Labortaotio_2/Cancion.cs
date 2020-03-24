cc965347cce782c29d753c8c0511614877473a3c
benjamorgan <bamorgan@miuandes.cl>
2020-03-24 17:14:51 -0300

    incremento 1, clases creadas

---
+++ Labortaotio_2/Labortaotio_2/Cancion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labortaotio_2
+{
+    class Cancion
+    {
+        public string Nombre;
+        public string Album;
+        public string Artista;
+        public string Genero;
+
+        public Cancion(string aNombre, string aAlbum, string aArtista, string aGenero)
+        {
+            Nombre = aNombre;
+            Album = aAlbum;
+            Artista = aArtista;
+            Genero = aGenero;
+        }
+        public string Informacion()
+        {
+            return "genero: " + Genero + ", artista: " + Artista + ", album: " + Album + ", nombre: " + Nombre;
+        }
+    }
+}
---
+++ Labortaotio_2/Labortaotio_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labortaotio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido Nuevo usuario, que desea hacer hoy?");
+            Console.WriteLine("Ver todas las canciones (1)");
+            Console.WriteLine("Agregar una cancion (2)");
+            Console.WriteLine("Salir del programa (9)");
+            string menu = Console.ReadLine();
+
+
+            
+
+
+
+            Cancion cancion1 = new Cancion("Red Moon", "Another Ark", "Uroboros", "Rock");
+            Console.WriteLine(cancion1.Informacion());
+
+
+            Console.ReadLine();
+        }
+    }
+}
